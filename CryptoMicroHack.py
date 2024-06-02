56a5753cf1386b4972fcd15fd8fc9030e3e07e49
zuzannab5 <56296468+zuzannab5@users.noreply.github.com>
2024-06-02 20:57:51 +0200

    Dodanie hashowania flag

--- CryptoMicroHack.py
+++ CryptoMicroHack.py
@@ -1,6 +1,13 @@
 
 import subprocess
-import sys
+import hashlib
+
+
+
+def hash(input_flag):
+    encoded_flag = input_flag.encode()
+    obj_sha3_256 = hashlib.sha3_256(encoded_flag)
+    return obj_sha3_256.hexdigest()
 
 class App():
     def __init__(self):
@@ -10,19 +17,23 @@ class App():
         prompt = "Podaj flagę:\n"
         while True:
             input_flag = input(prompt)
-            if input_flag == flag:
-                print("Udało ci się :) !")
+            hash_input_flag = hash(input_flag)
+            if not flag:
+                break
+            if hash_input_flag == flag:
+                print("Udało ci się! :) ")
+                print()
                 break
             print("Podałeś złą flagę :( , spróbuj ponownie!")
     def __open_zadanie(self, path_to_task):
         subprocess.Popen([path_to_task],shell=True)
 
     def __zadania(self, level:int):
-        flag = ""
+        flag = None
 
         if level == 1:
             self.__open_zadanie("Dodatki\\Zadanie_1\\task.pdf")
-            flag = "Snowman jest super"
+            flag = "f9aa6798eb46c7e2da6189473d64f80350fd929d8274d225aa435d4ed95d2f43"
         elif level == 2:
             pass
         elif level == 3:
@@ -30,7 +41,7 @@ class App():
         elif level == 4:
             self.__open_zadanie("Dodatki\\Zadanie_4\\task.pdf")
             print("Do rozwiązania potrzebny będzie plik /Dodatki/Zadanie_4/aes.py")
-            flag = "\\x81"
+            flag = "56b9ba20a36f03233c6d8b8c934a4b24ed361007e6a881847481bfc47cbaefd9"
         elif level == 5:
             pass
         self.__wait_for_flag(flag)
@@ -47,8 +58,11 @@ class App():
             print(f"Zapnij pasy! Zaczynamy zadanie {level}!")
             self.__zadania(level)
             print("---------------------------------------------")
-        print("Gratulacje, udało ci się zrobić wszystkie zadania :)!")
-
+        print("Gratulacje! Udało ci się zrobić wszystkie zadania :)")
+        print()
+        with open("ending.txt", "r", encoding="utf-8") as f:
+            logo = f.read()
+        print(logo)
 if __name__ == "__main__":
     app = App()
     app.run()

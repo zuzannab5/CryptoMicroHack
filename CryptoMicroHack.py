c04dd9b63db482b7a87109c91422a0543445b5c9
wasiakaa <aleksandra.wasiak.stud@pw.edu.pl>
2024-06-03 05:25:35 +0200

    Zadanie 3

--- CryptoMicroHack.py
+++ CryptoMicroHack.py
@@ -35,15 +35,17 @@ class App():
             self.__open_zadanie("Dodatki\\Zadanie_1\\task.pdf")
             flag = "f9aa6798eb46c7e2da6189473d64f80350fd929d8274d225aa435d4ed95d2f43"
         elif level == 2:
-            pass
+            self.__open_zadanie("Dodatki\\Zadanie_2\\task.pdf")
+            flag = "763d2d42a62138d3dc7e1e5d503b9333f33a562939a60493e403f0f1d107e3d4"
         elif level == 3:
-            pass
+            self.__open_zadanie("Dodatki\\Zadanie_3\\task.pdf")
+            flag = "3a4137897522d5db7d3138b23397830e60f3d4d255e515c2218b39bf747072e4"
         elif level == 4:
             self.__open_zadanie("Dodatki\\Zadanie_4\\task.pdf")
             print("Do rozwiązania potrzebny będzie plik /Dodatki/Zadanie_4/aes.py")
             flag = "56b9ba20a36f03233c6d8b8c934a4b24ed361007e6a881847481bfc47cbaefd9"
         elif level == 5:
-             self.__open_zadanie("Dodatki\\Zadanie_5\\task.pdf")
+            self.__open_zadanie("Dodatki\\Zadanie_5\\task.pdf")
             flag = "c72777c0da0231f1fc9a3fd753fec3a73543b3ae01aa67c259a9a643c4ae4e5e"
         self.__wait_for_flag(flag)
         
